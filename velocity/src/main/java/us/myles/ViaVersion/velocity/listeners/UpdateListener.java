a9026666d01252ccb4543c1a8f3d692ba85129e0
VytskaLT <VytskaLT@protonmail.com>
2020-12-04 14:54:56 +0200

    Don't need bungee, velocity, sponge or bukkit-legacy

--- velocity/src/main/java/us/myles/ViaVersion/velocity/listeners/UpdateListener.java
+++
@@ -1,8 +0,0 @@
-package us.myles.ViaVersion.velocity.listeners;
-
-import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.connection.PostLoginEvent;
-import us.myles.ViaVersion.api.Via;
-
-public class UpdateListener {
-}
